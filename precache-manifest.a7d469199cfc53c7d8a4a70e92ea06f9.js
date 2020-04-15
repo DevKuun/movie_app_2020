3f2ff2842d7e76872ef9a4aa2d3bd60a434193a0
kuun <kuun@kuuns-MacBook-Pro.local>
2020-04-15 16:51:08 +0900

    Updates

---
+++ precache-manifest.a7d469199cfc53c7d8a4a70e92ea06f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cbf755da48b4d480e2b319ddb0e375",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "dc029507f5c96936722b",
+    "url": "/movie_app_2020/static/css/main.52933d24.chunk.css"
+  },
+  {
+    "revision": "39b8bcc7d2d72670fac8",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc029507f5c96936722b",
+    "url": "/movie_app_2020/static/js/main.6a1d6934.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
